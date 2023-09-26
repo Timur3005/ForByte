75c0e82abc38b8a7180fe279e8c0612028060a98
Timur Makhmutov <makhmutov.timur2016@yandex.ru>
2023-09-26 23:26:03 +0300

    is done

--- app/src/main/java/edu/timurmakhmutov/forbyte/presentation/MainActivity.kt
+++ app/src/main/java/edu/timurmakhmutov/forbyte/presentation/MainActivity.kt
@@ -1,13 +1,15 @@
 package edu.timurmakhmutov.forbyte.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 import edu.timurmakhmutov.forbyte.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
     }
 }
